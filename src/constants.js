bcddef2f4e828d8d708e26271cf1444ab2ae34c7
Kevark <kevin@carlabs.com>
2018-10-12 13:57:52 -0700

    added hey

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // this syntax is overkill for these simple  examples, but extensible
 module.exports = (function() {
-  const greetings = ['hello', 'hi', 'yo', 'whatup', 'hola'];
-  const helpMessage = 'I am your fiendly port namer bot. You can ask me stuff like:\n```What port does telnet use?\nport 22?\nssh?\nGet me a random port\nLets play a game\nQuiz me```\nSo, what would you like to do ? :)';
+  const greetings = ['hello', 'hi', 'yo', 'whatup', 'hola', 'hey'];
+  const helpMessage = 'I am your friendly port namer bot. You can ask me stuff like:\n```What port does telnet use?\nport 22?\nssh?\nGet me a random port\nLets play a game\nQuiz me```\nSo, what would you like to do ? :)';
 
   return {
     greetings,
